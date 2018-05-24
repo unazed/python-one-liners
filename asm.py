249c04049ed9649524b609b480fb843776191797
Unazed Spectaculum <leethax0r.coder@gmail.com>
2018-05-24 20:52:00 +0000

    small update

--- asm.py
+++ asm.py
@@ -11,31 +11,59 @@ setvar("Register", type("Register", (object,), {
 })),
 setvar("Variable", type("Variable", (object,), {"__init__": lambda self, name, content=None:(setattr(self, "content", content),setattr(self, "name", name),None)[-1]})),
 setvar("Immediate", type("Immediate", (object,), {"__init__": lambda self, content: setattr(self, "content", content)})),
-setvar("parse_instruction", lambda i: [{"opcode": inst[0],"operands": inst[1:]} for inst in [i.replace(",", " ").split()] if inst][0]),
+setvar("parse_instruction", lambda i:
+{"opcode": i.split()[0],
+ "operands":
+ (
+ setvar("_operands", [""]),
+ [
+    (
+        setvar("_string", ""),
+        setvar("_first", True),
+        setvar("_running", True),
+        _operands.__setitem__(-1, _operands[-1]+([
+            setvar("_first", _first^1) if s == '"' else setvar("_running", False) if _first and s == ',' else setvar("_string", _string+s) if not _first else None
+            for s in ''.join(i.split()[1:])[' '.join(i.split()[1:]).index(c+1):]
+            if _running
+        ]
+        _string)[-1][:-1]) if c == '"' else None if c == " " else [] 
+    )
+    for c in ' '.join(i.split()[1:])
+ ] if len(i.split()) != 1 else (),
+ )[-1]
+}
+),
 setvar("imp", lambda n: setvar(n, __import__(n), False)),
 setvar("operand_map", {
-    ("Register", Register): tuple([Register(reg, None) for reg in ("eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi")]),
+    ("Register", Register): tuple([Register(reg, None) for reg in ("eax", "ecx", "edx", "ebx", "esp", "ebp", "esi", "edi", "flags")]),
     ("Variable", Variable): set()
 }),
 setvar("print_register", lambda name: ret if True in [(setvar("ret", reg.content), True)[1] if reg.name == name else None for reg in operand_map[('Register', Register)]] else sys.exit("error: no such register %r" % name)),
 setvar("print_variable", lambda name: [print(var.content) if var.name == name else None for var in operand_map[('Variable', Variable)]]),
 setvar("opcode_typemap", {
-    (Variable , Variable ): ["mov"],
-    (Variable , Immediate): ["mov", "add"],
-    (Variable , Register ): ["mov", "add"],
-    (Immediate, Variable ): False,
+    (Variable, Variable): ["mov"],
+    (Variable, Immediate): ["mov", "add", "sub", "mul", "div", "pow", "call"],
+    (Variable, Register): ["mov", "add", "sub", "mul", "div", "pow"],
+    (Immediate, Variable): False,
     (Immediate, Immediate): False,
-    (Immediate, Register ): False,
-    (Register , Variable ): ["mov", "add"],
-    (Register , Immediate): ["mov", "add"],
-    (Register , Register ): ["mov", "add"],
-    (Register,): ["inc"],
-    (Variable,): ["inc"],
+    (Immediate, Register): False,
+    (Register, Variable): ["mov", "add", "sub", "mul", "div", "pow"],
+    (Register, Immediate): ["mov", "add", "sub", "mul", "div", "pow"],
+    (Register, Register): ["mov", "add", "sub", "mul", "div", "pow"],
+    (Register,): ["inc", "dec"],
+    (Variable,): ["inc", "dec"],
 }),
+setvar("stack", []),
 setvar("opcode_function_map", {
     "mov": lambda op1, op2: setattr(op1, "content", op2.content),
     "add": lambda op1, op2: setattr(op1, "content", int(op1.content)+int(op2.content)),
-    "inc": lambda op1: setattr(op1, "content", int(op1.content)+1)
+    "inc": lambda op1: setattr(op1, "content", int(op1.content)+1),
+    "dec": lambda op1: setattr(op1, "content", int(op1.content)-1),
+    "sub": lambda op1, op2: setattr(op1, "content", int(op1.content)-int(op2.content)),
+    "mul": lambda op1, op2: setattr(op1, "content", int(op1.content)*int(op2.content)),
+    "div": lambda op1, op2: setattr(op1, "content", int(op1.content)//int(op2.content)),
+    "pow": lambda op1, op2: setattr(op1, "content", int(op1.content)**int(op2.content)),
+    "call": lambda op1, op2: globals()[op1.content](*[*[stack.pop() for _ in range(int(op2.content))]]) if len(stack) >= int(op2.content) else sys.exit("error: requested %r variables, but only got %r" % (op2.content, len(stack)))
 }),
 setvar("identify_operand_type", 
 lambda operand, define_labels=False:
@@ -60,9 +88,7 @@ sfile.close()
 setvar("parsed_line", parse_instruction(line)),
 setvar("parsed_line", {parsed_line['opcode']: [identify_operand_type(op, True) for op in parsed_line['operands']]}),
 setvar("line_template", ([*parsed_line.keys()][0], *map(type, [*parsed_line.values()][0]))),
-print(line_template),
 setvar("generated_opcode_map", generate_opcode_map(opcode_typemap, opcode_function_map)),
 sys.exit("error: operand type mismatch for %r" % parsed_line) if (setvar("opcode", generated_opcode_map.get(line_template, None)), opcode)[1] is None else None,
 opcode(*[*parsed_line.values()][0]),
-breakpoint()
 )
